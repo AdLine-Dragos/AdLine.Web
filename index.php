af8c0feeebd6742614c268cdd203e5ab9e6f4a5f
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-06 17:46:36 +0300

    Small tweaks to the header

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 <!-- Main Body -->
 <?php include 'header.html' ?>
 
+
 <!-- Scripts -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
