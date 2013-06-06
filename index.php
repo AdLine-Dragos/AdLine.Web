3ce82ce01d9369d5f710d0dda4094c46f73b533b
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-06 22:04:50 +0300

    Added Navigation
    Added Navigation Bar to the top, collapsing when under 1024
    Changed the Styles to LESS
    Overall Additions

--- index.php
+++ index.php
@@ -2,26 +2,39 @@
 <html>
 <head>
 <meta charset="utf-8">
-
 <title>AdLine.Web - Choose your template and create your website!</title>
 
 <!-- StyleSheets -->
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
-
+<link rel="stylesheet/less" type="text/css" href="css/styles.less"/>
 </head>
 
 <body>
 <!-- Main Body -->
+<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+				<div class="container">
+						<button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+						<div class="nav-collapse collapse" style="height: 0px;">
+								<ul class="nav">
+										<li class=""> <a href="#">Home</a> </li>
+										<li class=""> <a href="#">About</a> </li>
+										<li class=""> <a href="#">Contact</a> </li>
+								</ul>
+						</div>
+				</div>
+		</div>
+</div>
+
 <?php include 'header.html' ?>
 
 
-<!-- Scripts -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<!-- Scripts --> 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script> 
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
+<script src="js/less-1.3.3.min.js" type="text/javascript"></script> 
+<script src="js/bootstrap.min.js" type="text/javascript"></script> 
 <script src="js/customJS.js" type="text/javascript"></script>
-
 </body>
 </html>
