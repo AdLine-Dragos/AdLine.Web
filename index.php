f3ecd6c6556fa1d6f519ab8d440e241c684edb83
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-07 18:10:40 +0300

    Worked on the slider and responsiveness

--- index.php
+++ index.php
@@ -9,78 +9,76 @@
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet/less" type="text/css" href="css/styles.less"/>
 <link rel="stylesheet" href="css/layerslider.css" type="text/css">
-
 </head>
 
 <body>
 <!-- Main Body -->
 <div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="navbar-inner">
-				<div class="container">
-						<button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-						<a class="brand" href="index.php">Menu</a>
-						<div class="nav-collapse collapse" style="height: 0px;">
-								<ul class="nav">
-										<li class=""> <a href="#">Home</a> </li>
-										<li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">About<b class="caret"></b></a>
-										<ul class="dropdown-menu">
-      <li><a href="#">Company</a>
-	  <a href="#">Work</a>
-    </ul></li>
-										<li class=""> <a href="#">Contact</a> </li>
-								</ul>
-								      <div class="pull-right" id="language"> 
-      	<div class="flag"></div>
-		<p>Change Language</p>
-      
+  <div class="navbar-inner">
+    <div class="container">
+      <button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="brand" href="index.php">Menu</a>
+      <div class="nav-collapse collapse" style="height: 0px;">
+        <ul class="nav">
+          <li class=""> <a href="#">Home</a> </li>
+          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">About<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Company</a> <a href="#">Work</a>
+            </ul>
+          </li>
+          <li class=""> <a href="#">Contact</a> </li>
+        </ul>
+        <div class="pull-right" id="language">
+          <div class="flag"></div>
+          <p>Change Language</p>
+        </div>
       </div>
-						</div>
-				</div>
-		</div>
+    </div>
+  </div>
 </div>
 <?php include 'header.html' ?>
 
-
 <!--LayerSlider begin-->
-<div id="slider" style="width: 80%; height: 400px;">
- 
-    <!--LayerSlider layer-->
-    <div class="ls-layer">
- 
-        <!--LayerSlider background-->
-        <img class="ls-bg" src="assets/l3.jpg" alt="layer1-background">
- 
-        <!--LayerSlider sublayers-->
-        <h3 class="ls-s2" alt="layer1-sublayer1" style="left: 50%; top: 50%;">Test 1</h3>
-        <h3 class="ls-s4" alt="layer1-sublayer2" style="left: 20%; top: 20%;">Test 2</h3>
-        <h3 class="ls-s6" alt="layer1-sublayer3" style="left: 80%; top: 80%;">Test 3</h3>
-    </div>
-	
-	<!--LayerSlider layer-->
-    <div class="ls-layer">
- 
-        <!--LayerSlider background-->
-        <img class="ls-bg" src="assets/image1.jpg" alt="layer1-background">
- 
-        <!--LayerSlider sublayers-->
-        <h3 class="ls-s1" alt="layer1-sublayer1" style="left: 50%; top: 50%;">Test 1</h3>
-        <h3 class="ls-s2" alt="layer1-sublayer2" style="left: 20%; top: 20%;">Test 2</h3>
-        <h3 class="ls-s3" alt="layer1-sublayer3" style="left: 80%; top: 80%;">Test 3</h3>
-    </div>
- 
-</div>
-<!--LayerSlider end-->
+<div id="slider" style="width: 80%; height: 400px;"> 
+  
+  <!--LayerSlider layer-->
+  <div class="ls-layer"> 
+    
+    <!--LayerSlider background--> 
+    <img class="ls-bg" src="assets/Charity.jpg" alt="layer1-background"> 
+    
+    <!--LayerSlider sublayers-->
+	<!--<div class="ls-s3 bg_white caption" alt="layer1-sublayer1" style="left:80%; top:80%;">The New Charity Template</div>-->
+  
+  <!--LayerSlider layer-->
+  <div class="ls-layer"> 
+    
+    <!--LayerSlider background--> 
+    <img class="ls-bg" src="assets/Charity2.jpg" alt="layer1-background"> 
+    
+    <!--LayerSlider sublayers-->
 
+  </div>
+    <div class="ls-layer"> 
+    
+    <!--LayerSlider background--> 
+    <img class="ls-bg" src="assets/Charity3.jpg" alt="layer1-background"> 
+    
+    <!--LayerSlider sublayers-->
+
+  </div>
+</div>
+<!--LayerSlider end--> 
 
 <!-- Scripts --> 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script> 
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script src="js/jquery-easing-1.3.js" type="text/javascript"></script>
-<script src="js/layerslider.kreaturamedia.jquery-min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
+<script src="js/jquery-easing-1.3.js" type="text/javascript"></script> 
+<script src="js/layerslider.kreaturamedia.jquery-min.js" type="text/javascript"></script> 
 <script src="js/less-1.3.3.min.js" type="text/javascript"></script> 
 <script src="js/bootstrap.min.js" type="text/javascript"></script> 
-<script src="js/customJS.js" type="text/javascript"></script>
+<script src="js/customJS.js" type="text/javascript"></script> 
 <script>$('#slider').layerSlider({
     autoStart               : true,
     firstLayer              : 1,
