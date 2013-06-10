a82a7e16f5f5b03f34fc1b0c586b1e5d17eac90e
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-10 18:42:50 +0300

    Changed Main Sliders
    Preparing for coding the mobile version

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
 <!-- StyleSheets -->
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/responsiveslides.css" type="text/css">
 <link rel="stylesheet/less" type="text/css" href="css/styles.less"/>
-<link rel="stylesheet" href="css/layerslider.css" type="text/css">
+<link rel="stylesheet" href="css/royal-slider-1.0.min.css" type="text/css">
+
 </head>
 
 <body>
@@ -17,19 +19,11 @@
 
 <?php include 'header.html' ?>
 
-<!--LayerSlider begin-->
-<div id="slider" style="width: 80%; height: 400px;"> 
-  <div class="ls-layer">  
-    <img class="ls-bg" src="assets/Charity.jpg" alt="layer1-background">
-  </div>
-  <div class="ls-layer"> 
-    <img class="ls-bg" src="assets/Charity2.jpg" alt="layer1-background"> 
-  </div>
-  <div class="ls-layer"> 
-    <img class="ls-bg" src="assets/Charity3.jpg" alt="layer1-background"> 
-  </div>
-</div>
-<!--LayerSlider end--> 
+<ul class="rslides hidden-phone" id="slider">
+  <li><img src="assets/Charity.jpg" alt=""></li>
+  <li><img src="assets/Charity2.jpg" alt=""></li>
+  <li><img src="assets/Charity3.jpg" alt=""></li>
+</ul>
 
 <!-- Short INFO - WHAT WE DO -->
 <div class="container-fluid">
@@ -42,18 +36,22 @@
 	<div class="row-fluid shortInfo">
 		<div class="span3 pagination-centered">
 		<img src="assets/what_we_do.png">
-		<p>Build websites for you so that you can focus more on developing your business.</p>
+		<h2>Create websites</h2>
+		<p>We create the websites for you so that you can focus more on developing your business.</p>
 		</div>
 		<div class="span3  pagination-centered">
 		<img src="assets/customize.png">
-		<p>Help you have a unique presence on the web by customizing your website. You won't have to move a finger!</p>
+		<h2>Customize them</h2>
+		<p>Have a unique presence on the web by customizing your website. You will not have to move a finger!</p>
 		</div>
 		<div class="span3 pagination-centered">
 		<img src="assets/documentation2.png">
+		<h2>Document them</h2>
 		<p>Provide a thorough documentation, ensuring an easy maintenance process afterwards.</p>
 		</div>
 		<div class="span3 pagination-centered">
 		<img src="assets/prof_service.png">
+		<h2>Deliver them</h2>
 		<p>Deliver a professional, high-quality product that will help you overcome the competition.</p>
 		</div>
 	</div>
@@ -68,19 +66,23 @@
 	<div class="row-fluid shortInfo">
 		<div class="span3 pagination-centered">
 		<img src="assets/fast_service.png">
-		<p>We offer a fast service! Our aim is to deliver your website in less than 3 days whenever possible!</p>
+		<h2>Fast-service</h2>
+		<p>Our aim is to deliver your website, with your help, in less than 3 days, whenever possible!</p>
 		</div>
 		<div class="span3  pagination-centered">
 		<img src="assets/no_headaches.png">
-		<p>Us taking care of everything means no headaches and more free time to enjoy for yourself.</p>
+		<h2>No headaches</h2>
+		<p>Us taking care of everything means more free time to enjoy for yourself.</p>
 		</div>
 		<div class="span3 pagination-centered">
 		<img src="assets/price-quality.png">
+		<h2>Price/Quality</h2>
 		<p>We guarantee the best price/quality ratio on the market. You can easily afford a website that will make you stand out!</p>
 		</div>
 		<div class="span3 pagination-centered">
 		<img src="assets/communication.png">
-		<p>We consider a good communication with our customers one of our top priorities. You can always contact us!</p>
+		<h2>Great support</h2>
+		<p>We consider a good communication with our customers the key to a successful collaboration.</p>
 		</div>
 	</div>
 </div>
@@ -90,50 +92,31 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script> 
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
 <script src="js/jquery-easing-1.3.js" type="text/javascript"></script> 
-<script src="js/layerslider.kreaturamedia.jquery-min.js" type="text/javascript"></script> 
+<script src="js/responsiveslides.min.js" type="text/javascript"></script>
+<script src="js/royal-slider-1.0.min.js" type="text/javascript"></script>
 <script src="js/less-1.3.3.min.js" type="text/javascript"></script> 
 <script src="js/bootstrap.min.js" type="text/javascript"></script> 
 <script src="js/customJS.js" type="text/javascript"></script> 
-<script>$('#slider').layerSlider({
-    autoStart               : true,
-    firstLayer              : 1,
-    twoWaySlideshow         : false,
-    keybNav                 : true,
-    touchNav                : true,
-    imgPreload              : true,
-    navPrevNext             : true,
-    navStartStop            : false,
-    navButtons              : false,
-    skin                    : 'defaultskin',
-    skinsPath               : 'css/skins/',
-    pauseOnHover            : true,
-    globalBGColor           : 'transparent',
-    globalBGImage           : false,
-    animateFirstLayer       : false,
-    yourLogo                : false,
-    yourLogoStyle           : 'position: absolute; z-index: 1001; left: 10px; top: 10px;',
-    yourLogoLink            : false,
-    yourLogoTarget          : '_blank',
-    loops                   : 0,
-    forceLoopNum            : true,
-    autoPlayVideos          : true,
-    autoPauseSlideshow      : 'auto',
-    youtubePreview          : 'maxresdefault.jpg',
- 
-    // you can change this settings separately by layers or sublayers with using html style attribute
- 
-    slideDirection          : 'right',
-    slideDelay              : 4000,
-    parallaxIn              : .45,
-    parallaxOut             : .45,
-    durationIn              : 1000,
-    durationOut             : 1000,
-    easingIn                : 'easeInOutQuint',
-    easingOut               : 'easeInOutQuint',
-    delayIn                 : 0,
-    delayOut                : 0
- 
+<script>
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: true,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: true,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "#slider",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "centered-btns",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
 });
 </script>
+
 </body>
 </html>
