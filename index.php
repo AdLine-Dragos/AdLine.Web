1f6df3da3132b213cf8438c72d8301672e7afc8a
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-10 20:12:49 +0300

    Deleted Royal Slider
    Made the mobile version with CSS

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="css/responsiveslides.css" type="text/css">
-<link rel="stylesheet/less" type="text/css" href="css/styles.less"/>
-<link rel="stylesheet" href="css/royal-slider-1.0.min.css" type="text/css">
+<link rel="stylesheet/less" type="text/css" href="css/styles.less" />
 
 </head>
 
@@ -29,27 +28,27 @@
 <div class="container-fluid">
 	<div class="row-fluid spacing">
 		<div class="span12">
-			<h4 class="red">WHAT WE DO</h4>
+			<h4 class="red sectionTitle">WHAT WE DO</h4>
 			<div class="full-width-separator"></div>
 		</div>
 	</div>
-	<div class="row-fluid shortInfo">
-		<div class="span3 pagination-centered">
+	<div class="row-fluid shortInfo clearfix">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/what_we_do.png">
 		<h2>Create websites</h2>
 		<p>We create the websites for you so that you can focus more on developing your business.</p>
 		</div>
-		<div class="span3  pagination-centered">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/customize.png">
 		<h2>Customize them</h2>
 		<p>Have a unique presence on the web by customizing your website. You will not have to move a finger!</p>
 		</div>
-		<div class="span3 pagination-centered">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/documentation2.png">
 		<h2>Document them</h2>
 		<p>Provide a thorough documentation, ensuring an easy maintenance process afterwards.</p>
 		</div>
-		<div class="span3 pagination-centered">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/prof_service.png">
 		<h2>Deliver them</h2>
 		<p>Deliver a professional, high-quality product that will help you overcome the competition.</p>
@@ -57,29 +56,29 @@
 	</div>
 	
 <!-- Short INFO - WHY US -->
-	<div class="row-fluid spacing">
+	<div class="row-fluid spacing clearfix">
 		<div class="span12">
-			<h4 class="red">WHY US</h4>
+			<h4 class="red sectionTitle">WHY US</h4>
 			<div class="full-width-separator"></div>
 		</div>
 	</div>
 	<div class="row-fluid shortInfo">
-		<div class="span3 pagination-centered">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/fast_service.png">
 		<h2>Fast-service</h2>
 		<p>Our aim is to deliver your website, with your help, in less than 3 days, whenever possible!</p>
 		</div>
-		<div class="span3  pagination-centered">
+		<div class="span3  pagination-centered clearfix">
 		<img src="assets/no_headaches.png">
 		<h2>No headaches</h2>
 		<p>Us taking care of everything means more free time to enjoy for yourself.</p>
 		</div>
-		<div class="span3 pagination-centered">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/price-quality.png">
 		<h2>Price/Quality</h2>
 		<p>We guarantee the best price/quality ratio on the market. You can easily afford a website that will make you stand out!</p>
 		</div>
-		<div class="span3 pagination-centered">
+		<div class="span3 pagination-centered clearfix">
 		<img src="assets/communication.png">
 		<h2>Great support</h2>
 		<p>We consider a good communication with our customers the key to a successful collaboration.</p>
@@ -93,7 +92,6 @@
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
 <script src="js/jquery-easing-1.3.js" type="text/javascript"></script> 
 <script src="js/responsiveslides.min.js" type="text/javascript"></script>
-<script src="js/royal-slider-1.0.min.js" type="text/javascript"></script>
 <script src="js/less-1.3.3.min.js" type="text/javascript"></script> 
 <script src="js/bootstrap.min.js" type="text/javascript"></script> 
 <script src="js/customJS.js" type="text/javascript"></script> 
@@ -117,6 +115,5 @@ $(".rslides").responsiveSlides({
   after: function(){}     // Function: After callback
 });
 </script>
-
 </body>
 </html>
--- js/customJS.js
+++ js/customJS.js
@@ -1,2 +1,22 @@
-// JavaScript Document
+$(function() {
+var getWindowWidth = $(window).width();
 
+if (getWindowWidth <= 767) {
+	$('.shortInfo div').removeClass("pagination-centered");
+}
+else 
+	$('.shortInfo div').addClass("pagination-centered");
+
+$(window).resize(function () {
+var windowwidth = $(window).width();
+function fixSlide() {
+	if (windowwidth <= 767) {
+		$('.shortInfo div').removeClass("pagination-centered");
+	}
+	else 
+	$('.shortInfo div').addClass("pagination-centered");
+}
+
+fixSlide();	
+});
+});
