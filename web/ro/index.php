03e08db66a1429ea972aa02a4339cfdab759f700
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-11 23:46:41 +0300

    Added romanian

---
+++ web/ro/index.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+<title>AdLine.Web - Alege-ti design-ul si creeaza-ti propriul website!</title>
+
+<!-- StyleSheets -->
+<link href="../en/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="../en/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="../en/css/responsiveslides.css" type="text/css">
+<link rel="stylesheet/less" type="text/css" href="../en/css/styles.less" />
+</head>
+
+<body>
+<!-- Main Body -->
+
+<?php include 'header.html' ?>
+<ul class="rslides hidden-phone" id="slider">
+  <li><img src="../en/assets/Charity.jpg" alt=""></li>
+  <li><img src="../en/assets/Charity2.jpg" alt=""></li>
+  <li><img src="../en/assets/Charity3.jpg" alt=""></li>
+</ul>
+
+<div class="visible-phone mobile-image" id="caption">
+<span>
+<a href="#"><h3>Intra in magazin!</h3></a>
+</span>
+<img class="lazy" src="../en/img/transparent.png" data-original="../en/assets/youview.jpg" width="767" height="588">
+</div>
+
+<!-- Short INFO - WHAT WE DO -->
+<div class="container-fluid">
+<div class="row-fluid spacing">
+  <div class="span12">
+    <h4 class="red sectionTitle">CE FACEM</h4>
+    <div class="full-width-separator"></div>
+  </div>
+</div>
+<div class="row-fluid shortInfo clearfix">
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/what_we_do.png">
+    <h2>Cream Site-uri</h2>
+    <p>Noi construim site-urile in locul tau pentru ca tu sa te poti concentra pe dezvoltarea afacerii tale.</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/customize.png">
+    <h2>Le personalizam</h2>
+    <p>Ne ocupam de personalizarea site-ului, iar tu nu va trebui sa misti un deget!</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/documentation2.png">
+    <h2>Te invatam</h2>
+    <p>Iti oferim o documentatie amanuntita, facand actualizarea mai usoara.</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/prof_service.png">
+    <h2>Ti-l trimitem</h2>
+    <p>Iti trimitem un produs final profesional si de calitate ridicata.</p>
+  </div>
+</div>
+<div id="slider1Holder">
+<div id="slider1" class="mobile-wwd"> <a class="buttons prev" href="#">stanga</a>
+  <div class="viewport">
+    <ul class="overview">
+      <li>
+        <div class="pagination-centered"> <img src="../en/assets/what_we_do.png">
+          <h2>Cream Site-uri</h2>
+          <p>Noi construim site-urile in locul tau pentru ca tu sa te poti concentra pe dezvoltarea afacerii tale.</p>
+        </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="../en/assets/customize.png">
+          <h2>Le personalizam</h2>
+          <p>Ne ocupam de personalizarea site-ului, iar tu nu va trebui sa misti un deget!</p>
+        </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="../en/assets/documentation2.png">
+          <h2>Te invatam</h2>
+          <p>Iti oferim o documentatie amanuntita, facand actualizarea mai usoara.</p>
+        </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="../en/assets/prof_service.png">
+          <h2>Ti-l trimitem</h2>
+          <p>Iti trimitem un produs final profesional si de calitate ridicata.</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <a class="buttons next" href="#">dreapta</a> </div>
+  </div>
+
+<!-- Short INFO - WHY US -->
+<div class="row-fluid spacing clearfix">
+  <div class="span12">
+    <h4 class="red sectionTitle">AVANTAJELE NOASTRE</h4>
+    <div class="full-width-separator"></div>
+  </div>
+</div>
+<div class="row-fluid shortInfo">
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/fast_service.png">
+    <h2>Serviciu rapid</h2>
+    <p>Obiectivul nostru este sa livram site-ul in maximum 3 zile, cu putin ajutor din partea ta.</p>
+  </div>
+  <div class="span3  pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/no_headaches.png">
+    <h2>Timp liber</h2>
+    <p>Iti oferim mai mult timp liber ocupandu-ne de totul in locul tau.</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/price-quality.png">
+    <h2>Pret/Calitate</h2>
+    <p>Garantam cel mai bun raport calitate/pret de pe piata. </p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img class="lazy" src="../en/img/transparent.png" width="122" height"105" data-original="../en/assets/communication.png">
+    <h2>Suport deosebit</h2>
+    <p>Consideram comunicarea cu clientul cheia unei bune colaborari, asa ca punem accent pe suportul pe care il oferim.</p>
+  </div>
+  </di>
+</div>
+<div id="slider2Holder">
+<div id="slider2" class="mobile-wwd"> <a class="buttons prev" href="#">stanga</a>
+  <div class="viewport">
+    <ul class="overview">
+      <li>
+         <div class="pagination-centered"> <img src="../en/assets/fast_service.png">
+    <h2>Serviciu rapid</h2>
+    <p>Obiectivul nostru este sa livram site-ul in maximum 3 zile, cu putin ajutor din partea ta.</p>
+  </div>
+      </li>
+      <li>
+       <div class="pagination-centered"> <img src="../en/assets/no_headaches.png">
+    <h2>Timp liber</h2>
+    <p>Iti oferim mai mult timp liber ocupandu-ne de totul in locul tau.</p>
+  </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="../en/assets/price-quality.png">
+    <h2>Pret/Calitate</h2>
+    <p>Garantam cel mai bun raport calitate/pret de pe piata.</p>
+  </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="../en/assets/communication.png">
+    <h2>Suport deosebit</h2>
+    <p>Consideram comunicarea cu clientul cheia unei bune colaborari, asa ca punem accent pe suportul pe care il oferim.</p>
+  </div>
+      </li>
+    </ul>
+  </div>
+  <a class="buttons next" href="#">dreapta</a> </div>
+  </div>
+  </div>
+<?php include 'footer.html' ?>
+
+<!-- Scripts --> 
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script> 
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
+<script src="../en/js/jquery.tinycarousel.min.js" type="text/javascript"></script> 
+<script src="../en/js/jquery-easing-1.3.js" type="text/javascript"></script> 
+<script src="../en/js/responsiveslides.min.js" type="text/javascript"></script> 
+<script src="../en/js/less-1.3.3.min.js" type="text/javascript"></script> 
+<script src="../en/js/bootstrap.min.js" type="text/javascript"></script> 
+<script src="../en/js/jquery.lazyload.min.js"></script>
+<script src="../en/js/customJS.js" type="text/javascript"></script> 
+<script>
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: true,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: true,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "#slider",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "centered-btns",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+</script> 
+<script>
+    $(function(){
+                      $('#slider1').tinycarousel();
+					  $('#slider2').tinycarousel();
+        $("img.lazy").lazyload({
+			threshold: 0,
+			effect : "fadeIn"
+		});
+    });
+</script>
+</body>
+</html>
