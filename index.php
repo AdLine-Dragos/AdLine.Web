da607c149214a0946f728a861846f1cbe476d148
AdLine-Dragos <dai4@kent.ac.uk>
2013-06-11 12:44:52 +0300

    Added carousel for Short Info sections

--- index.php
+++ index.php
@@ -10,14 +10,12 @@
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="css/responsiveslides.css" type="text/css">
 <link rel="stylesheet/less" type="text/css" href="css/styles.less" />
-
 </head>
 
 <body>
 <!-- Main Body -->
 
 <?php include 'header.html' ?>
-
 <ul class="rslides hidden-phone" id="slider">
   <li><img src="assets/Charity.jpg" alt=""></li>
   <li><img src="assets/Charity2.jpg" alt=""></li>
@@ -26,73 +24,130 @@
 
 <!-- Short INFO - WHAT WE DO -->
 <div class="container-fluid">
-	<div class="row-fluid spacing">
-		<div class="span12">
-			<h4 class="red sectionTitle">WHAT WE DO</h4>
-			<div class="full-width-separator"></div>
-		</div>
-	</div>
-	<div class="row-fluid shortInfo clearfix">
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/what_we_do.png">
-		<h2>Create websites</h2>
-		<p>We create the websites for you so that you can focus more on developing your business.</p>
-		</div>
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/customize.png">
-		<h2>Customize them</h2>
-		<p>Have a unique presence on the web by customizing your website. You will not have to move a finger!</p>
-		</div>
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/documentation2.png">
-		<h2>Document them</h2>
-		<p>Provide a thorough documentation, ensuring an easy maintenance process afterwards.</p>
-		</div>
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/prof_service.png">
-		<h2>Deliver them</h2>
-		<p>Deliver a professional, high-quality product that will help you overcome the competition.</p>
-		</div>
-	</div>
-	
-<!-- Short INFO - WHY US -->
-	<div class="row-fluid spacing clearfix">
-		<div class="span12">
-			<h4 class="red sectionTitle">WHY US</h4>
-			<div class="full-width-separator"></div>
-		</div>
-	</div>
-	<div class="row-fluid shortInfo">
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/fast_service.png">
-		<h2>Fast-service</h2>
-		<p>Our aim is to deliver your website, with your help, in less than 3 days, whenever possible!</p>
-		</div>
-		<div class="span3  pagination-centered clearfix">
-		<img src="assets/no_headaches.png">
-		<h2>No headaches</h2>
-		<p>Us taking care of everything means more free time to enjoy for yourself.</p>
-		</div>
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/price-quality.png">
-		<h2>Price/Quality</h2>
-		<p>We guarantee the best price/quality ratio on the market. You can easily afford a website that will make you stand out!</p>
-		</div>
-		<div class="span3 pagination-centered clearfix">
-		<img src="assets/communication.png">
-		<h2>Great support</h2>
-		<p>We consider a good communication with our customers the key to a successful collaboration.</p>
-		</div>
-	</div>
+<div class="row-fluid spacing">
+  <div class="span12">
+    <h4 class="red sectionTitle">WHAT WE DO</h4>
+    <div class="full-width-separator"></div>
+  </div>
 </div>
+<div class="row-fluid shortInfo clearfix">
+  <div class="span3 pagination-centered clearfix"> <img src="assets/what_we_do.png">
+    <h2>Create websites</h2>
+    <p>We create the websites for you so that you can focus more on developing your business.</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img src="assets/customize.png">
+    <h2>Customize them</h2>
+    <p>Have a unique presence on the web by customizing your website. You will not have to move a finger!</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img src="assets/documentation2.png">
+    <h2>Document them</h2>
+    <p>Provide a thorough documentation, ensuring an easy maintenance process afterwards.</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img src="assets/prof_service.png">
+    <h2>Deliver them</h2>
+    <p>Deliver a professional, high-quality product that will help you overcome the competition.</p>
+  </div>
+</div>
+<div id="slider1Holder">
+<div id="slider1" class="mobile-wwd"> <a class="buttons prev" href="#">left</a>
+  <div class="viewport">
+    <ul class="overview">
+      <li>
+        <div class="pagination-centered"> <img src="assets/what_we_do.png">
+          <h2>Create websites</h2>
+          <p>We create the websites for you so that you can focus more on developing your business.</p>
+        </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="assets/customize.png">
+          <h2>Customize them</h2>
+          <p>Have a unique presence on the web by customizing your website. You will not have to move a finger!</p>
+        </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="assets/documentation2.png">
+          <h2>Document them</h2>
+          <p>Provide a thorough documentation, ensuring an easy maintenance process afterwards.</p>
+        </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="assets/prof_service.png">
+          <h2>Deliver them</h2>
+          <p>Deliver a professional, high-quality product that will help you overcome the competition.</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <a class="buttons next" href="#">right</a> </div>
+  </div>
+
+<!-- Short INFO - WHY US -->
+<div class="row-fluid spacing clearfix">
+  <div class="span12">
+    <h4 class="red sectionTitle">WHY US</h4>
+    <div class="full-width-separator"></div>
+  </div>
+</div>
+<div class="row-fluid shortInfo">
+  <div class="span3 pagination-centered clearfix"> <img src="assets/fast_service.png">
+    <h2>Fast-service</h2>
+    <p>Our aim is to deliver your website, with your help, in less than 3 days, whenever possible!</p>
+  </div>
+  <div class="span3  pagination-centered clearfix"> <img src="assets/no_headaches.png">
+    <h2>No headaches</h2>
+    <p>Us taking care of everything means more free time to enjoy for yourself.</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img src="assets/price-quality.png">
+    <h2>Price/Quality</h2>
+    <p>We guarantee the best price/quality ratio on the market. You can easily afford a website that will make you stand out!</p>
+  </div>
+  <div class="span3 pagination-centered clearfix"> <img src="assets/communication.png">
+    <h2>Great support</h2>
+    <p>We consider a good communication with our customers the key to a successful collaboration.</p>
+  </div>
+  </di>
+</div>
+<div id="slider2Holder">
+<div id="slider2" class="mobile-wwd"> <a class="buttons prev" href="#">left</a>
+  <div class="viewport">
+    <ul class="overview">
+      <li>
+         <div class="pagination-centered"> <img src="assets/fast_service.png">
+    <h2>Fast-service</h2>
+    <p>Our aim is to deliver your website, with your help, in less than 3 days, whenever possible!</p>
+  </div>
+      </li>
+      <li>
+       <div class="pagination-centered"> <img src="assets/no_headaches.png">
+    <h2>No headaches</h2>
+    <p>Us taking care of everything means more free time to enjoy for yourself.</p>
+  </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="assets/price-quality.png">
+    <h2>Price/Quality</h2>
+    <p>We guarantee the best price/quality ratio on the market. You can easily afford a website that will make you stand out!</p>
+  </div>
+      </li>
+      <li>
+        <div class="pagination-centered"> <img src="assets/communication.png">
+    <h2>Great support</h2>
+    <p>We consider a good communication with our customers the key to a successful collaboration.</p>
+  </div>
+      </li>
+    </ul>
+  </div>
+  <a class="buttons next" href="#">right</a> </div>
+  </div>
 <?php include 'footer.html' ?>
 
 <!-- Scripts --> 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script> 
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
+<script src="js/jquery.tinycarousel.min.js" type="text/javascript"></script> 
 <script src="js/jquery-easing-1.3.js" type="text/javascript"></script> 
-<script src="js/responsiveslides.min.js" type="text/javascript"></script>
+<script src="js/responsiveslides.min.js" type="text/javascript"></script> 
 <script src="js/less-1.3.3.min.js" type="text/javascript"></script> 
 <script src="js/bootstrap.min.js" type="text/javascript"></script> 
 <script src="js/customJS.js" type="text/javascript"></script> 
@@ -115,6 +170,13 @@ $(".rslides").responsiveSlides({
   before: function(){},   // Function: Before callback
   after: function(){}     // Function: After callback
 });
+</script> 
+<script>
+    $(function(){
+                      $('#slider1').tinycarousel();
+					  $('#slider2').tinycarousel();
+        
+    });
 </script>
 </body>
 </html>
--- js/customJS.js
+++ js/customJS.js
@@ -1,20 +1,42 @@
 $(function() {
 var getWindowWidth = $(window).width();
 
-if (getWindowWidth <= 767) {
+
+if (getWindowWidth <= 767 && getWindowWidth >= 361) {
+	$('.shortInfo').css('display', 'block');
 	$('.shortInfo div').removeClass("pagination-centered");
+	$('.mobile-wwd').css('display', 'none');
 }
-else 
+else if (getWindowWidth <=360)
+	{
+		$('.shortInfo').css('display', 'none');
+		$('.mobile-wwd').css('display', 'block');
+	}
+else {
+	$('.shortInfo').css('display', 'block');
 	$('.shortInfo div').addClass("pagination-centered");
+	$('.mobile-wwd').css('display', 'none');
+}
+	
 
 $(window).resize(function () {
 var windowwidth = $(window).width();
 function fixSlide() {
-	if (windowwidth <= 767) {
+	if (windowwidth <= 767 && windowwidth >= 361) {
+		$('.shortInfo').css('display', 'block');
 		$('.shortInfo div').removeClass("pagination-centered");
+		$('.mobile-wwd').css('display', 'none');
 	}
-	else 
+	else if (windowwidth <=360)
+	{
+		$('.shortInfo').css('display', 'none');
+		$('.mobile-wwd').css('display', 'block');
+	}
+	else {
+	$('.shortInfo').css('display', 'block');
 	$('.shortInfo div').addClass("pagination-centered");
+	$('.mobile-wwd').css('display', 'none');
+	}
 }
 
 fixSlide();	
